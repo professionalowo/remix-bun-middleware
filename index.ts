d9d54722dbaae3f36c746f9c4779be0019e41392
professionalowo <martin.brandl1307@gmail.com>
2024-07-20 19:18:43 +0200

    added jsdoc comment

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
 import type { MiddlewareHandler } from "hono";
 import { createRequestHandler, type CreateRequestHandlerArgs } from "bun-remix";
 
+/**
+ * 
+ * @param args {@link CreateRequestHandlerArgs}
+ * @returns a {@link MiddlewareHandler} that proxies requests to a Remix app
+ * 
+ * @example
+ * ```ts
+ *  const app = new Hono();
+ * 
+ *  const build = import(
+ *  "../build/server/index.js"
+ *  ) as unknown as Promise<ServerBuild>;
+ *
+ *  app.all(
+ *      await remixMiddleware({
+ *        build,
+ *        mode: Bun.env.NODE_ENV as "production" | "development",
+ *      }),
+ *  );
+ * ```
+ */
 export async function remixMiddleware(
   args: CreateRequestHandlerArgs
 ): Promise<MiddlewareHandler> {
